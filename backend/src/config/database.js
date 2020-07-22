d490a99928806ad61eb46ee2d26017d65379c2e9
hugo-marcelo <hugo.marcelo@lg.com.br>
2020-07-22 15:52:22 -0300

    chore: prepared backend to upload on heroku

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,12 +2,15 @@ require('../bootstrap');
 
 module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
-  host: process.env.DB_HOST,
+  dialectOptions: {
+    ssl: process.env.DB_SSL,
+    rejectUnauthorized: false,
+  },
+  host: process.env.DB_HOST || '127.0.0.1',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   storage: './__tests__/database.sqlite',
-  logging: true,
   define: {
     timestamps: true,
     underscored: true,
